de64b0a5f011f60fe0ff896a081ebf51cbcdb06e
Areg Amirjanyan <areg@Aregs-MacBook-Pro.local>
2023-02-13 23:51:05 +0400

    message

---
+++ src/hw8/store/polls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import StoreCategory, ItemCategory, Customer, StoreOwner, Store, Item, MyBug, Purchase
+
+admin.site.register(StoreCategory)
+admin.site.register(ItemCategory)
+admin.site.register(Customer)
+admin.site.register(StoreOwner)
+admin.site.register(Store)
+admin.site.register(Item)
+admin.site.register(MyBug)
+admin.site.register(Purchase)
+
+
+
